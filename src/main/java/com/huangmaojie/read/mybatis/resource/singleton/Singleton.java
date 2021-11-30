9f1054ac2909c6d0215550aeaab192fdc7bad725
huangmaojie <18292493938@163.com>
2021-11-30 17:33:13 +0800

    补充

--- src/main/java/com/huangmaojie/read/mybatis/resource/singleton/Singleton.java
+++ src/main/java/com/huangmaojie/read/mybatis/resource/singleton/Singleton.java
@@ -8,10 +8,10 @@ public class Singleton {
 
     //饿汉模式
     public static class Singleton1 {
-        //饿汉模 式是最简单的实现方式，在类加载的时候就创建了羊例类的对象
+        //饿汉模式是最简单的实现方式，在类加载的时候就创建了单例类的对象
         private static final Singleton instance = new Singleton();
 
-        //单例类的构造方法都是私有的，防止外部创建羊伽l类的对象
+        //单例类的构造方法都是私有的，防止外部创建单例类的对象
         private Singleton1() {
         }
 
@@ -22,17 +22,16 @@ public class Singleton {
 
     //懒汉模式
     public static class Singleton2 {
-        //饿汉模 式是最简单的实现方式，在类加载的时候就创建了羊例类的对象
+        //饿汉模式是最简单的实现方式，在类加载的时候就创建了单例类的对象
         private static Singleton2 instance = null;
 
-        //单例类的构造方法都是私有的，防止外部创建羊伽l类的对象
+        //单例类的构造方法都是私有的，防止外部创建单例类的对象
         private Singleton2() {
         }
 
         public static Singleton2 newInstance() {
-            // 在需要的时候才去创建的单例对象，如采羊例对象已经创建，
-            // 再次调用 ηewInstance()方法时
-            // 将不会重新创建新的单例对象，而是直接返回之前创建的单例对象
+            // 在需要的时候才去创建的单例对象，如果单例对象已经创建，
+            // 再次调用newInstance()方法时将不会重新创建新的单例对象，而是直接返回之前创建的单例对象
             if (null == instance) {
                 instance = new Singleton2();
                 return instance;
@@ -48,7 +47,7 @@ public class Singleton {
         private Singleton3() {
         }
 
-        //使用 synchronized 为 newinstance () 方法加锁
+        //使用synchronized为newInstance()方法加锁
         public static synchronized Singleton newInstance() {
             if (null == instance) {
                 instance = new Singleton();
@@ -66,8 +65,10 @@ public class Singleton {
         }
 
         public static Singleton4 getInstance() {
-            if (instance == null) {//第 一次检测
-                synchronized (Singleton4.class) {//加锁 if (instance == null) {//第二次检测
+            // 第一次检测
+            if (instance == null) {
+                // 第二次检测
+                synchronized (Singleton4.class) {
                     instance = new Singleton4();
                     return instance;
 
@@ -80,7 +81,7 @@ public class Singleton {
 
     // 安全的双检查锁
     public static class Singleton5 {
-        // 使用 volatile 关键字修饰 instance 字段
+        // 使用volatile关键字修饰instance字段
         private static volatile Singleton5 instance = null;
 
         private Singleton5() {
@@ -88,9 +89,9 @@ public class Singleton {
 
         public static Singleton5 getInstance() {
             if (instance == null) {
-                //第 一次检测
+                //第一次检测
                 synchronized (Singleton5.class) {
-                    // 加锁 if (instance == null) {//第二次检测
+                    // 第二次检测
                     instance = new Singleton5();
                     return instance;
 
@@ -101,7 +102,7 @@ public class Singleton {
     }
 
     public static class Singleton6 {
-        //私有的静态内部类，该静态内部类只会在newInstance()方法中被使用
+        // 私有的静态内部类，该静态内部类只会在newInstance()方法中被使用
         private static class SingletonHolder {
             //静态字段
             public static Singleton6 instance = new Singleton6();
